6bd8fc2441462c16ace1f5aa30d8e9a8bb32d31c
MarcosOki <marcosokita181@gmail.com>
2024-11-17 00:28:36 -0300

    Troquei o type do id

--- prisma/migrations/20241116183818_inicial/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id_post" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
-    "author" TEXT NOT NULL
-);
---
+++ prisma/migrations/20241117031649_att/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id_post" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
